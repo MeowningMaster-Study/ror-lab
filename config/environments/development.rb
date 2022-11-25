4c159adc74e423df8bf485b7173c2805ca18edfa
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2022-11-25 17:49:46 +0000

    fix codespaces

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "meowningmaster-supreme-happiness-jq7jx6prpgqfpwrx-3000.preview.app.github.dev"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
