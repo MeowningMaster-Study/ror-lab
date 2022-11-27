ec3ce64b0a08a30fe2dd83efa4c93b00d2028461
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2022-11-27 11:11:09 +0000

    final

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,11 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+
+    @t1 = t1(@students)
+    @t2 = t2(@students)
+    @t3 = t3(@students)
+    @t4 = t4(@students)
   end
 
   def show
@@ -46,4 +51,93 @@ class StudentsController < ApplicationController
     def student_params
       params.require(:student).permit(:name, :surname, :patronymic, :sex, :age, :course)
     end
+
+  # Номер курса з найбільшим процентом чоловіків
+  def t1(arr)
+    # Hash<course, [M, F]>
+    stats = Hash.new
+
+    arr.each do |s|
+      course = s.course
+      i = s.sex == "M" ? 0 : 1
+      unless stats.has_key?(course)
+        stats[course] = [0, 0]
+      end
+      stats[course][i] += 1
+    end
+
+    max_course = -Float::INFINITY
+    max_part = -Float::INFINITY
+    stats.each_pair do |course, counts|
+      m, f  = counts
+      part = m.to_f / (m + f)
+      if part > max_part
+        max_course = course
+        max_part = part
+      end
+    end
+
+    max_course
+  end
+
+  # Найрозповсюдженіші чоловічі та жіночі імена
+  def t2(arr)
+    # [M: Hash<name, count>, F: Hash<name, count>]
+    stats = [Hash.new, Hash.new]
+
+    arr.each do |s|
+      name = s.name
+      i = s.sex == "M" ? 0 : 1
+      stats_sex = stats[i]
+      unless stats_sex.has_key?(name)
+        stats_sex[name] = 0
+      end
+      stats_sex[name] += 1
+    end
+
+    stats.map do |stats_sex|
+      max_name = "None"
+      max_count = -Float::INFINITY
+      stats_sex.each_pair do |name, count|
+        if count > max_count
+          max_name = name
+          max_count = count
+        end
+      end
+      max_name
+    end
+  end
+
+  # Прізвища в алфавітному порядку
+  def t3(arr)
+    arr.map{|x| x.surname }.sort
+  end
+
+  # Ініціали студенток з найрозповсюдженішим віком
+  def t4(arr)
+    # Hash<age, count>
+    stats = Hash.new
+
+    arr.each do |s|
+      if s.sex == "F"
+        age = s.age
+        unless stats.has_key?(age)
+          stats[age] = 0
+        end
+        stats[age] += 1
+      end
+    end
+
+    age_mode = -Float::INFINITY
+    max_count = -Float::INFINITY
+    stats.each_pair do |age, count|
+      if count > max_count
+        max_count = count
+        age_mode = age
+      end
+    end
+
+    arr.select { |s| s.sex == "F" && s.age == age_mode }
+       .map{ |s| "%s %s. %s." % [s.surname, s.name[0], s.patronymic[0]] }
+  end
 end
